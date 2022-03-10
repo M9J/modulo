262eadaec92154101d17f6176767a9722832e3a6
mnsh.exe@gmail.com <mnsh.exe@gmail.com>
2022-03-10 16:22:38 +0530

    async fixes

--- src/core/runner.js
+++ src/core/runner.js
@@ -1,22 +1,34 @@
-export default function run(modules) {
+export default async function run(modules) {
   const hasModules = Array.isArray(modules) 
     ? modules.length > 0 
     : false;
   if (hasModules) {
-    modules.forEach(m => {
-      let isEnabled = false;
-      if (m.metadata) {
-        isEnabled = m.metadata.isEnabled;
-      }
-      if (isEnabled) {
-        console.log(`===== ${m.metadata.name} =====`);
-        if (m.main && typeof m.main === "function") {
-          m.main();
-        }
-      }
-      
+    runModules(modules);
+  }
+}
+
+async function runModules(modules) {
+  if (modules) {
+    modules.forEach(async (module) => {
+      await runModule(module);
     });
   }
-  
+}
+
+async function runModule(module) {
+  if (module) {
+      let isEnabled = false;
+      if (module.metadata) {
+        isEnabled = module.metadata.isEnabled;
+      }
+      if (isEnabled) {
+        console.log(`===== ${module.metadata.name} =====`);
+        if (module.main && typeof module.main === "function") {
+          module.main();
+        }
+      }
+  }
+  await new Promise(r => setTimeout(r, 3000));
+  return true;
   
 }
