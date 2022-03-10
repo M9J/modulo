6f728e437094d687ea98ba07f08b4fa341718fcf
mnsh.exe@gmail.com <mnsh.exe@gmail.com>
2022-03-11 01:24:39 +0530

    minor fixes

--- src/modules/arithmetic/arithmetic.js
+++ src/modules/arithmetic/arithmetic.js
@@ -5,5 +5,10 @@ import { Division } from './division.js';
 
 export class Arithmetic {
   name = 'Arithmetic';
-  modules = [Addition, Substraction, Multiplication, Division];
+  modules = [
+    Addition, 
+    Substraction, 
+    Multiplication, 
+    Division
+  ];
 }
