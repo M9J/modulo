0397f77e1d83e2a6673eadc10a1a3d93f8ff181f
mnsh.exe@gmail.com <mnsh.exe@gmail.com>
2022-03-13 02:16:18 +0530

    test commit 2

--- modulo/modulo.js
+++ modulo/modulo.js
@@ -2,6 +2,7 @@ import Runner from './core/runner.js';
 import Modules from '../modules/modules.js';
 
 // Runner
-new Runner().runModules(
+const runner = new Runner();
+runner.runModules(
   Modules.reverse()
 );
