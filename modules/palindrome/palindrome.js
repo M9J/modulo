4601c15d2f04106f455396c40ced50b738a6b883
George M. Jay <mnsh.exe@gmail.com>
2022-03-13 15:13:31 +0530

    Palindrome fixes

--- modules/palindrome/palindrome.js
+++ modules/palindrome/palindrome.js
@@ -4,7 +4,7 @@ export class Palindrome {
 
   main() {
     let seq = [0, 1];
-    const next = () => seq[seq.length - 1] + seq[seq.length - 2];
+    const next = () => seq.at(-1) + seq.at(-2);
     while (next() < 1000) seq.push(next());
     console.log(seq);
   }
