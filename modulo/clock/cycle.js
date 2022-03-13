41d1aedffeabd155114ad14319e8fc3625d49615
George M. Jay <mnsh.exe@gmail.com>
2022-03-13 15:30:35 +0530

    FPS fixes

--- modulo/clock/cycle.js
+++ modulo/clock/cycle.js
@@ -9,7 +9,7 @@ export default function cycle(ms = FPS1) {
 
 const FPS = (fps = 1) => 1000 / fps;
 export const FPS1 = FPS(1);
-export const FPS10 = FPS(100);
-export const FPS60 = FPS(16.66);
-export const FPS100 = FPS(10);
-export const FPS120 = FPS(8.33);
+export const FPS10 = FPS(10);
+export const FPS60 = FPS(60);
+export const FPS100 = FPS(100);
+export const FPS120 = FPS(120);
